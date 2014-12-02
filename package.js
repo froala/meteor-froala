6119bca8f85dde7d3918031a9f059e13fa59b3e1
Stefan Neculai <stefan.neculai@gmail.com>
2014-12-02 16:08:36 +0200

    Remove langs. Update package.

--- package.js
+++ package.js
@@ -13,47 +13,16 @@ Package.onUse(function(api) {
   ], "client");
 
   api.addFiles([
+    // CSS
     "froala:editor/css/froala_content.css",
     "froala:editor/css/froala_editor.css",
     "froala:editor/css/froala_style.css",
 
+    // JS
     "froala:editor/js/froala_editor.min.js",
     "froala:editor/js/froala_editor_ie8.min.js",
-    
-    "froala:editor/js/langs/ar.js",
-    "froala:editor/js/langs/bs.js",
-    "froala:editor/js/langs/cs.js",
-    "froala:editor/js/langs/da.js",
-    "froala:editor/js/langs/de.js",
-    "froala:editor/js/langs/en_ca.js",
-    "froala:editor/js/langs/en_gb.js",
-    "froala:editor/js/langs/en_us.js",
-    "froala:editor/js/langs/es.js",
-    "froala:editor/js/langs/fa.js",
-    "froala:editor/js/langs/fi.js",
-    "froala:editor/js/langs/fr.js",
-    "froala:editor/js/langs/he.js",
-    "froala:editor/js/langs/hr.js",
-    "froala:editor/js/langs/hu.js",
-    "froala:editor/js/langs/id.js",
-    "froala:editor/js/langs/it.js",
-    "froala:editor/js/langs/ja.js",
-    "froala:editor/js/langs/ko.js",
-    "froala:editor/js/langs/me.js",
-    "froala:editor/js/langs/nb.js",
-    "froala:editor/js/langs/nl.js",
-    "froala:editor/js/langs/pl.js",
-    "froala:editor/js/langs/pt_br.js",
-    "froala:editor/js/langs/pt_pt.js",
-    "froala:editor/js/langs/ro.js",
-    "froala:editor/js/langs/ru.js",
-    "froala:editor/js/langs/sr.js",
-    "froala:editor/js/langs/sv.js",
-    "froala:editor/js/langs/th.js",
-    "froala:editor/js/langs/tr.js",
-    "froala:editor/js/langs/zh_cn.js",
-    "froala:editor/js/langs/zh_tw.js",
 
+    // JS Plugins
     "froala:editor/js/plugins/block_styles.min.js",
     "froala:editor/js/plugins/char_counter.min.js",
     "froala:editor/js/plugins/colors.min.js",
