5c2a6ac7b0e1a54273622920319f30cd03b623c9
Stefan Neculai <stefan.neculai@gmail.com>
2015-10-07 14:02:56 +0300

    Update to V2 RC3.

--- package.js
+++ package.js
@@ -1,39 +1,60 @@
 Package.describe({
   name: "froala:editor",
   summary: "A beautiful jQuery WYSIWYG HTML rich text editor.",
-  version: "1.2.8",
+  version: "2.0.0-rc.3",
   git: "https://github.com/froala/meteor-froala/"
 });
 
 Package.onUse(function(api) {
   api.use([
-    "jquery@1.0.1",
-    "fortawesome:fontawesome@4.2.0"
+    "jquery@1.11.0",
+    "fortawesome:fontawesome@4.4.0"
   ], "client");
 
   api.addFiles([
     // CSS
-    "froala:editor/css/froala_content.min.css",
     "froala:editor/css/froala_editor.min.css",
     "froala:editor/css/froala_style.min.css",
 
+    // CSS Plugins
+    "froala:editor/css/char_counter.min.css",
+    "froala:editor/css/code_view.min.css",
+    "froala:editor/css/colors.min.css",
+    "froala:editor/css/emoticons.min.css",
+    "froala:editor/css/file.min.css",
+    "froala:editor/css/fullscreen.min.css",
+    "froala:editor/css/image_manager.min.css",
+    "froala:editor/css/image.min.css",
+    "froala:editor/css/line_breaker.min.css",
+    "froala:editor/css/table.min.css",
+    "froala:editor/css/video.min.css",
+
     // JS
     "froala:editor/js/froala_editor.min.js",
-    "froala:editor/js/froala_editor_ie8_wrapped.min.js",
 
     // JS Plugins
-    "froala:editor/js/plugins/block_styles.min.js",
+    "froala:editor/js/plugins/align.min.js",
     "froala:editor/js/plugins/char_counter.min.js",
+    "froala:editor/js/plugins/code_view.min.js",
     "froala:editor/js/plugins/colors.min.js",
-    "froala:editor/js/plugins/file_upload.min.js",
+    "froala:editor/js/plugins/emoticons.min.js",
+    "froala:editor/js/plugins/entities.min.js",
+    "froala:editor/js/plugins/file.min.js",
     "froala:editor/js/plugins/font_family.min.js",
     "froala:editor/js/plugins/font_size.min.js",
     "froala:editor/js/plugins/fullscreen.min.js",
-    "froala:editor/js/plugins/inline_styles.min.js",
+    "froala:editor/js/plugins/image_manager.min.js",
+    "froala:editor/js/plugins/image.min.js",
+    "froala:editor/js/plugins/inline_style.min.js",
+    "froala:editor/js/plugins/line_breaker.min.js",
+    "froala:editor/js/plugins/link.min.js",
     "froala:editor/js/plugins/lists.min.js",
-    "froala:editor/js/plugins/media_manager.min.js",
-    "froala:editor/js/plugins/tables.min.js",
-    "froala:editor/js/plugins/urls.min.js",
+    "froala:editor/js/plugins/paragraph_format.min.js",
+    "froala:editor/js/plugins/paragraph_style.min.js",
+    "froala:editor/js/plugins/quote.min.js",
+    "froala:editor/js/plugins/save.min.js",
+    "froala:editor/js/plugins/table.min.js",
+    "froala:editor/js/plugins/url.min.js",
     "froala:editor/js/plugins/video.min.js"
   ], ["client"]);
 });
