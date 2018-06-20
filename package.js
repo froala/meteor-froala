bfe31df8b4d1fa51fbffcfd0ab79034d01a88d77
Stefan Neculai <stefan.neculai@gmail.com>
2018-06-20 19:53:24 +0300

    Update to 2.8.2.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "froala:editor",
   summary: "A beautiful jQuery WYSIWYG HTML rich text editor.",
-  version: "2.8.1",
+  version: "2.8.2",
   git: "https://github.com/froala/meteor-froala/"
 });
 
