547a7b54502095cd9acb60299d08a9138508f53e
Navneet Kumar <navneet.kumar@accoliteindia.com>
2019-06-06 15:56:15 +0530

    Update to 3.0.0-rc.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "froala:editor",
   summary: "A beautiful Javascript WYSIWYG HTML rich text editor.",
-  version: "3.0.0-rc.1",
+  version: "3.0.0-rc.2",
   git: "https://github.com/froala/meteor-froala/"
 });
 
