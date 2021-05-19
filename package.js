f43b901cc56c98851944f8a646345417095329c2
Kapil Garg <kapil.garg.techy@gmail.com>
2021-05-19 11:38:51 +0530

    Updated to v3.2.7

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "froala:editor",
   summary: "A beautiful Javascript WYSIWYG HTML rich text editor.",
-  version: "3.2.6-1",
+  version: "3.2.7",
   git: "https://github.com/froala/meteor-froala/"
 });
 
