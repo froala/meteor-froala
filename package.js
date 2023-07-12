df805a197cf6ff8239c999d380306b6409e749f6
harasunu-narayan <60644555+harasunu-narayan@users.noreply.github.com>
2023-07-12 17:15:18 +0530

    Update to v4.1.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "froala:editor",
   summary: "A beautiful Javascript WYSIWYG HTML rich text editor.",
-  version: "4.0.19",
+  version: "4.1.0",
   git: "https://github.com/froala/meteor-froala/"
 });
 
