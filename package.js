82c80de2b89ba404355cec8f0fa326a8329be20f
harasunu-narayan <harasunu.narayan@celestialsys.com>
2025-07-23 17:59:07 +0530

    Update to v4.6.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "froala:editor",
   summary: "A beautiful Javascript WYSIWYG HTML rich text editor.",
-  version: "4.5.2",
+  version: "4.6.0",
   git: "https://github.com/froala/meteor-froala/"
 });
 
